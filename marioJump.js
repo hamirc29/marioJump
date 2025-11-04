75b5769b5eba48fc669e5ea09609f094a185e24d
hamirc29 <hamirc29@episcopalacademy.org>
2025-11-04 09:30:17 -0500

    Add files via upload

---
+++ marioJump.js
@@ -0,0 +1,251 @@
+var play, coin, bullet, special, regen, ground;
+
+var pX, pY, pSize;
+var pVx = 0, pVy = 0;
+
+var cX, cY, cSize, cVx, cVy;
+var bX, bY, bSize, bVx;
+var sX, sY, sSize, sVy;
+var rX, rY, rSize, rVx, rVy;
+
+var score = 0, lives = 7;
+
+var groundY;
+var jumpTopY;
+
+function setup()
+{
+    createCanvas(windowWidth, windowHeight)
+  frameRate(60);
+
+  groundY = height - height / 6;
+
+  pSize = (width * height) / (9000.0 * (width / 900.0));
+  pX = random(0, width);
+  pY = groundY;
+  jumpTopY = groundY - height / 3;
+
+  cSize = (width * height) / (13000.0 * (width / 900.0));
+  cX = random(0, width);
+  cY = random(0, groundY - cSize);
+  cVx = random(5, 15) * (width / 900.0);
+  cVy = random(5, 15) * (height / 700.0);
+
+  bSize = (width * height) / (7000.0 * (width / 900.0));
+  bX = random(0, width);
+  bY = random(0, groundY - bSize);
+  bVx = random(10, 20) * (width / 900.0);
+
+  sSize = (width * height) / (7000.0 * (width / 900.0));
+  sX = random(0, width);
+  sY = random(0, groundY - sSize);
+  sVy = 2 * (height / 700.0);
+
+  rSize = (width * height) / (12000.0 * (width / 900.0));
+  rX = random(0, width);
+  rY = random(0, groundY - rSize);
+  rVx = 10 * (width / 900.0);
+  rVy = 10 * (height / 700.0);
+
+  play = loadImage("mario.png");
+  coin = loadImage("mariocoin.png");
+  bullet = loadImage("bulletbill.png");
+  special = loadImage("bombomb.png");
+  regen = loadImage("upmushroom.png");
+}
+
+function draw()
+{
+  background(0, 50, 255);
+  noStroke();
+
+  fill(0, 255, 0);
+  rect(0, groundY, width, height / 6);
+
+  pY += pVy;
+  pVy += 0.5 * (height / 700.0);
+
+  if (pVy < 0 && pY <= jumpTopY)
+  {
+    pVy = 5 * (height / 700.0);
+  }
+
+  if (pY >= groundY)
+  {
+    pY = groundY;
+    pVy = 0;
+  }
+
+  if (!keyPressed)
+  {
+    pVx = 0;
+  }
+
+  pX += pVx;
+
+  image(play, pX - pSize / 2, pY - pSize, pSize, pSize);
+  image(coin, cX, cY, cSize, cSize);
+  image(bullet, bX, bY, bSize, bSize);
+  image(special, sX, sY, sSize, sSize);
+  if (lives < 2)
+  {
+    image(regen, rX, rY, rSize, rSize);
+  }
+
+  cX += cVx;
+  cY -= cVy;
+  bX += bVx;
+  sY += sVy;
+  rX += rVx;
+  rY += rVy;
+
+  textAlign(LEFT);
+  textSize(50 * (height / 700.0));
+  fill(255);
+  text("Score: " + score, width / 16, height / 12);
+  textSize(25 * (height / 700.0));
+  text("Lives: " + lives, width / 16, height / 6);
+
+  float playerCenterX = pX;
+  float playerCenterY = pY - pSize / 2;
+
+  float coinCenterX = cX + cSize / 2;
+  float coinCenterY = cY + cSize / 2;
+
+  float bulletCenterX = bX + bSize / 2;
+  float bulletCenterY = bY + bSize / 2;
+
+  float bombCenterX = sX + sSize / 2;
+  float bombCenterY = sY + sSize / 2;
+
+  float regenCenterX = rX + rSize / 2;
+  float regenCenterY = rY + rSize / 2;
+
+  float distC = dist(playerCenterX, playerCenterY, coinCenterX, coinCenterY);
+  float distB = dist(playerCenterX, playerCenterY, bulletCenterX, bulletCenterY);
+  float distS = dist(playerCenterX, playerCenterY, bombCenterX, bombCenterY);
+  float distR = dist(playerCenterX, playerCenterY, regenCenterX, regenCenterY);
+
+  if (distC < (pSize + cSize) / 2)
+  {
+    score += 10;
+    cX = random(0, width);
+    cY = random(0, groundY - cSize);
+  }
+
+  if (distB < (pSize + bSize) / 2)
+  {
+    lives -= 1;
+    bX = random(0, width);
+    bY = random(0, groundY - bSize);
+    bVx = 12 * (width / 900.0); 
+  }
+
+  if (distS < (pSize + sSize) / 2)
+  {
+    lives -= 2;
+    sX = random(0, width);
+    sY = 0;
+    sVy = 2 * (height / 700.0); 
+  }
+
+  if (lives < 2 && distR < (pSize + rSize) / 2)
+  {
+    lives += 4;
+    rX = random(0, width);
+    rY = random(0, groundY - rSize);
+  }
+
+  if (cX > width || cX < 0)
+  {
+    cVx *= -1;
+  }
+
+  if (cY > groundY - cSize / 2 || cY < 0)
+  {
+    cVy *= -1;
+  }
+
+  if (bX > width)
+  {
+    bX = 0;
+    bY = random(0, groundY - bSize);
+    bVx = 12 * (width / 900.0);
+  }
+
+  if (sY > groundY - sSize )
+  {
+    sX = random(0, width);
+    sY = 0;
+    sVy = 2 * (height / 700.0);
+  }
+
+  if (rX > width || rX < 0)
+  {
+    rVx *= -1;
+  }
+
+  if (rY > groundY - rSize / 2 || rY < 0)
+  {
+    rVy *= -1;
+  }
+
+  if (pX > width)
+  {
+    pX = 0;
+  }
+
+  if (pX < 0)
+  {
+    pX = width;
+  }
+
+  if (score > 140)
+  {
+    background(255);
+    fill(0, 255, 0);
+    textSize(width / 5.3);
+    textAlign(CENTER);
+    text("YOU WIN", width / 2, height / 2);
+    if (keyCode == ENTER)
+    {
+      score = 0;
+      lives = 7;
+    }
+  }
+
+  if (lives < 1)
+  {
+    background(0);
+    fill(255, 0, 0);
+    textSize(width / 8);
+    textAlign(CENTER);
+    text("GAME OVER", width / 2, height / 2);
+    if (keyCode == ENTER)
+    {
+      score = 0;
+      lives = 7;
+    }
+  }
+}
+
+function keyPressed()
+{
+  if (key == CODED)
+  {
+    if (keyCode == UP && pY >= groundY)
+    {
+      pVy = -15 * (height / 700.0);
+    }
+
+    if (keyCode == LEFT)
+    {
+      pVx = -12 * (width / 900.0);
+    }
+
+    if (keyCode == RIGHT)
+    {
+      pVx = 12 * (width / 900.0);
+    }
+  }
+}
